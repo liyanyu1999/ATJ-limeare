5501000aa871d28edb640a1d7f4a565df045e1bb
zpl <grontylion@foxmail.com>
2022-11-15 19:02:36 +0800

    feat(zpl): initialization

---
+++ src/main/java/com/yihuolu/atj/AtjApplication.java
@@ -0,0 +1,16 @@
+package com.yihuolu.atj;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.yihuolu.atj.mapper")
+public class AtjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yihuolu/atj/AtjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yihuolu.atj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
